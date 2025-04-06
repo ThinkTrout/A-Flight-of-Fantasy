bb5dd643674e6b357c432b0098aca1bfcfca5e63
Sir ThinkTrout <chengxi.li.usa@gmail.com>
2025-04-06 01:47:19 +0000

    fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def handle_command(cmd):
         return f"You try to '{cmd}', but nothing happens."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
